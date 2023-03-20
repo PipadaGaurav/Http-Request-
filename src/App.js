c06d2dc7081e1024f3fedb3301443b37e1726eff
PipadaGaurav <2019pcecsgaurav58@poornima.org>
2023-03-20 15:25:46 +0530

    Added post request using firebase database

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React ,{useState,useCallback,useEffect} from 'react';
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
         setIsLoading(true);
         setError(null);
         try {
-          const response = await fetch('https://swapi.dev/api/films/');
+          const response = await fetch('https://react-http-practise-88791-default-rtdb.firebaseio.com/movies.json');
           if (!response.ok) {
             throw new Error('Something went wrong!');
           }
@@ -22,16 +23,19 @@ function App() {
              // fetch('https://swapi.dev/api/films/').then(response => {
             //   return response.json();
             // }).then((data) => {
+
+            const loadedMovies = [];
+
+            for (const key in data) {
+              loadedMovies.push({
+                id: key,
+                title: data[key].title,
+                openingText: data[key].openingText,
+                releaseDate: data[key].releaseDate,
+              });
+            }
     
-          const transformedMovies = data.results.map((movieData) => {
-            return {
-              id: movieData.episode_id,
-              title: movieData.title,
-              openingText: movieData.opening_crawl,
-              releaseDate: movieData.release_date,
-            };
-          });
-          setMovies(transformedMovies);
+          setMovies(loadedMovies);
         } catch (error) {
           setError(error.message);
         }
@@ -41,6 +45,20 @@ function App() {
       useEffect(() => {
         fetchMoviesHandler();
       }, [fetchMoviesHandler]);
+
+
+     async function addMovieHandler(movie) {
+         const response= await fetch('https://react-http-practise-88791-default-rtdb.firebaseio.com/movies.json',{
+            method:'POST',
+            body: JSON.stringify(movie),
+            headers:{
+                'Content-Type':'application/json'
+            }
+          });
+          const data = await response.json;
+          console.log(data);
+  }
+
 
       let content = <p>Found no movies.</p>;
 
@@ -59,11 +77,12 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler}/>
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
